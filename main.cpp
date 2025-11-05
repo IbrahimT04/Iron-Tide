c81677187ccfb9287af318beb8fa8f0d5faa4a20
Ibrahim Tariq <itariq640@gmail.com>
2025-11-04 22:57:16 -0500

    Setup SDL3

--- main.cpp
+++ main.cpp
@@ -1,16 +1,66 @@
-#include <iostream>
+#include <stdio.h>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char* argv[])
+{
+    if (SDL_Init(SDL_INIT_VIDEO) == false)
+    {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Couldn't initialize SDL!", SDL_GetError(), NULL);
+        return SDL_APP_FAILURE;
     }
 
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+    // 800x450 is 16:9
+    if (SDL_CreateWindowAndRenderer("hello SDL3 with cmake", 800, 450, 0, &window, &renderer) == false)
+    {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Couldn't create window/renderer!", SDL_GetError(), NULL);
+        return SDL_APP_FAILURE;
+    }
+
+    // return success!
+    return SDL_APP_CONTINUE;
+}
+
+// This function runs when a new event occurs
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    switch (event->type)
+    {
+        case SDL_EVENT_QUIT:
+            // end the program, reporting success to the OS
+            return SDL_APP_SUCCESS;
+        case SDL_EVENT_KEY_DOWN:
+            if (event->key.key == SDLK_ESCAPE)
+            {
+                // end the program on ESC key,
+                // returning success to the OS
+                return SDL_APP_SUCCESS;
+            }
+        default:
+            break;
+    }
+
+    // return continue to continue
+    return SDL_APP_CONTINUE;
+}
+
+// This function runs once per frame, and is the heart of the program
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    SDL_SetRenderDrawColor(renderer, 135, 206, 235, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    // return continue to continue
+    return SDL_APP_CONTINUE;
+}
+
+// This function runs once at shutdown
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    // SDL will clean up the window/renderer for us.
 }
